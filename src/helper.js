f9dee15b63db132f90586affdb7f3a35d1703212
Darshan <CS115K41@ms.mcehassan.ac.in>
2024-04-07 08:03:53 +0530

    mentor and mentee first step

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,2 @@
-export const BASE_URL = "http://localhost:3000";
-
-// export const BASE_URL = "https://mentornmentee.onrender.com";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://mentornmentee.onrender.com";
